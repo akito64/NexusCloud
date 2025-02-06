a7bcb570facb83ed8e855cd45fb4720cea7a0a6c
604-24\ktc <>
2025-02-06 16:30:52 +0900

    Updated UI and tested deployment

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to NexusCloud</h1>
+      <p>This is a test deployment of our Amplify project.</p>
+      <button onClick={() => alert("Hello!")}>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
